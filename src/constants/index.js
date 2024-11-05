e07ad01efd6b5a8aea2cde29361435aa9d59608f
Kateryna Davidovych <kathylevchenko1@gmail.com>
2024-11-05 14:48:51 +0200

    + port

--- src/constants/index.js
+++ src/constants/index.js
@@ -13,6 +13,7 @@ export const ROLES = {
 
 export const SMTP = {
   SMTP_HOST: 'SMTP_HOST',
+  SMTP_PORT: 'SMTP_PORT',
   SMTP_USER: 'SMTP_USER',
   SMTP_PASSWORD: 'SMTP_PASSWORD',
   SMTP_FROM: 'SMTP_FROM',
