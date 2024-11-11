083b5a03ea798f22a900bb1dfa78a8859c5f4bfd
Kateryna Davidovych <kathylevchenko1@gmail.com>
2024-11-11 12:25:54 +0200

    change env

--- src/utils/googleOAuth2.js
+++ src/utils/googleOAuth2.js
@@ -4,14 +4,14 @@ import { readFile } from 'fs/promises';
 import createHttpError from 'http-errors';
 import { env } from './env.js';
 
-const PATH_JSON = path.join(process.cwd(), 'google-oauth.json');
+// const PATH_JSON = path.join(process.cwd(), 'google-oauth.json');
 
-const oauthConfig = JSON.parse(await readFile(PATH_JSON));
+// const oauthConfig = JSON.parse(await readFile(PATH_JSON));
 
 const googleOAuthClient = new OAuth2Client({
   clientId: env('GOOGLE_AUTH_CLIENT_ID'),
   clientSecret: env('GOOGLE_AUTH_CLIENT_SECRET'),
-  redirectUri: oauthConfig.web.redirect_uris[0],
+  redirectUri: env('redirect_uris'),
 });
 
 export const generateAuthUrl = () =>
